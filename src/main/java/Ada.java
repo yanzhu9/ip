c2febc0a184c02cc03032d8cb90ea506d80bfa4c
Yanzhu Chen <e1513114@u.nus.edu>
2026-08-20 20:40:13 +0800

    Level 2 done

--- src/main/java/Ada.java
+++ src/main/java/Ada.java
@@ -9,22 +9,34 @@ public class Ada {
                         " / ___ \\ | |_| | / ___ \\ \n" +
                         "/_/   \\_\\\\____/ /_/   \\_\\\n";
         Scanner scanner = new Scanner(System.in);
+        String[] storage = new String[100];
+        int index = 0;
+
         System.out.println("---------------------------------------------------");
         System.out.print(banner);
-        System.out.println("Hello, I'm Ada.");
-        System.out.println("What can I do for you?");
+        System.out.println(" Hello, I'm Ada.");
+        System.out.println(" What can I do for you?");
         System.out.println("---------------------------------------------------");
         while(true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 System.out.println("---------------------------------------------------");
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("---------------------------------------------------");
                 break;
+            }else if(input.equals("list")){
+                System.out.println("---------------------------------------------------");
+                for(int i = 0; i < index; i ++){
+                    int j = i + 1;
+                    System.out.println(j + ". " + storage[i]);
+                }
+                System.out.println("---------------------------------------------------");
+            }else {
+                storage[index++] = input;
+                System.out.println("---------------------------------------------------");
+                System.out.println(" added: " + input);
+                System.out.println("---------------------------------------------------");
             }
-            System.out.println("---------------------------------------------------");
-            System.out.println(input);
-            System.out.println("---------------------------------------------------");
         }
     }
 }
