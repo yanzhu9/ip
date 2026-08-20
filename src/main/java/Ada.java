47df30b0a2ad770e85a0e810343988fd825a4ba2
Yanzhu Chen <e1513114@u.nus.edu>
2026-08-20 20:24:20 +0800

    Level 1 done

--- src/main/java/Ada.java
+++ src/main/java/Ada.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ada {
     public static void main(String[] args) {
         String banner =
@@ -6,12 +8,23 @@ public class Ada {
                         "  / _ \\  | | | |  / _ \\  \n" +
                         " / ___ \\ | |_| | / ___ \\ \n" +
                         "/_/   \\_\\\\____/ /_/   \\_\\\n";
-        System.out.print("---------------------------------------------------\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---------------------------------------------------");
         System.out.print(banner);
         System.out.println("Hello, I'm Ada.");
         System.out.println("What can I do for you?");
-        System.out.print("---------------------------------------------------\n");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.print("---------------------------------------------------");
+        System.out.println("---------------------------------------------------");
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("---------------------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("---------------------------------------------------");
+                break;
+            }
+            System.out.println("---------------------------------------------------");
+            System.out.println(input);
+            System.out.println("---------------------------------------------------");
+        }
     }
 }
